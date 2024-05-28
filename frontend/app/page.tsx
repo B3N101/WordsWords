74b5bdc912ecf2eb12791efcf72b924bdd7fdfed
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2024-05-28 01:26:34 -0400

    updates

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-   <p className="text-green-600 font-extrabold text-xl">Home</p>
-  );
+  return <p className="text-green-600 font-extrabold text-xl">Home</p>;
 }
