2ad17b355bc7378cb8d61ab47e65ea32679d70ba
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2024-05-27 23:19:59 -0400

    added analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Analytics } from "@vercel/analytics/react";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -13,6 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
+      <Analytics />
       <body
         className={cn(
           "min-h-screen bg-background bg-red-300 dark:bg-red-950 font-sans antialiased",
