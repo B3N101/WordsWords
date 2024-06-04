f0b4307a700edbdf8c81dbd3e13c78732a5ab355
Ian1528 <102779366+Ian1528@users.noreply.github.com>
2024-06-03 19:24:55 -0500

    Revert "Auth2"

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,13 @@
-import { auth } from "@/auth/index";
-import AuthButton from "./AuthButton.server";
+import { SignIn } from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+import Image from "next/image";
 
-export default async function Home() {
-  const session = await auth();
+export default function Home() {
   return (
     <div>
+      <SignIn />
+      <SignOut />
       <p className="text-green-600 font-extrabold text-xl">Home</p>
-      <AuthButton />
-      <p>Session: {JSON.stringify(session)}</p>
     </div>
   );
 }
