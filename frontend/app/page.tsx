96e879b79ac9332852252f06c0e7252419043c57
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2024-06-07 13:02:54 -0400

    auth init - no db

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { auth } from "@/auth/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+
   return (
     <div>
       <p className="text-green-600 font-extrabold text-xl">Home</p>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,4 @@
-const { fontFamily } = require("tailwindcss/defaultTheme")
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -11,4 +11,4 @@ module.exports = {
       },
     },
   },
-}
+};
