a89bff8661407a9348929bb830b4a4db4fd01582
Ian1528 <102779366+Ian1528@users.noreply.github.com>
2024-06-10 15:44:45 +0100

    updated layout imports

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import "@/app/globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import NavBar from "@/components/NavBar";
