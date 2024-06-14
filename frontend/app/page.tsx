57dedbc42a1f594d5c05fd29d11f2a93f76bdace
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2024-06-14 19:06:26 -0400

    auth init - db v0

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
 import { auth } from "@/auth/auth";
+import { redirect } from "next/navigation";
+import Profile from "@/components/profile";
 
 export default async function Home() {
   const session = await auth();
 
+  if (!session) redirect("/api/auth/signin");
+
+
+  // const User = {
+  //   name: session.user.name,
+  //   username: session.user.username,
+  //   image: session.user.image,
+  // };
 
   return (
     <div>
       <p className="text-green-600 font-extrabold text-xl">Home</p>
       <pre>{JSON.stringify(session, null, 2)}</pre>
+      {/* <Profile user={User} /> */}
     </div>
   );
 }
--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -2,15 +2,3 @@ import { loadEnvConfig } from "@next/env";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
-
-const config = {
-  POSTGRES_URL: process.env.POSTGRES_URL!,
-  AUTH_SECRET: process.env.AUTH_SECRET!,
-  AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID!,
-  AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET!,
-  AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID!,
-  AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET!,
-  APP_ENV: process.env.APP_ENV!,
-};
-
-export default config;
