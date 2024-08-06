22943f74450f6e2683519a715db821abdfbd2a18
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2024-08-06 16:46:03 -0400

    updates w/ schema & seed

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -49,3 +49,4 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </html>
   );
 }
+ 
--- frontend/prisma/queries.ts
+++ frontend/prisma/queries.ts
@@ -1,24 +1,24 @@
 import {
   PrismaClient,
-  PublishedList,
-  UserQuizProgress,
+  // PublishedList,
+  // UserQuizProgress,
   UserWordsListProgress,
   Word,
   WordsList,
 } from "@prisma/client";
-import { UserQuizProgressWithQuiz } from "@/prisma/types";
+// import { UserQuizProgressWithQuiz } from "@/prisma/types";
 
 const prisma = new PrismaClient();
 
-export async function getUserWordLists(
-  userID: string,
-): Promise<PublishedList[]> {
-  const data = await prisma.publishedList.findMany({
-    where: { userId: userID },
-    include: { list: true },
-  });
-  return data;
-}
+// export async function getUserWordLists(
+//   userID: string,
+// ): Promise<PublishedList[]> {
+//   const data = await prisma.publishedList.findMany({
+//     where: { userId: userID },
+//     include: { list: true },
+//   });
+//   return data;
+// }
 
 export async function getWordList(
   wordListID: string,
@@ -30,16 +30,16 @@ export async function getWordList(
   return data;
 }
 
-export async function getUserWordListProgress(
-  userID: string,
-  wordListID: string,
-) {
-  const data = await prisma.userWordsListProgress.findFirst({
-    where: { userId: userID, wordsListListId: wordListID },
-    include: { userQuizProgresses: true },
-  });
-  return data;
-}
+// export async function getUserWordListProgress(
+//   userID: string,
+//   wordListID: string,
+// ) {
+//   const data = await prisma.userWordsListProgress.findFirst({
+//     where: { userId: userID, wordsListListId: wordListID },
+//     include: { userQuizProgresses: true },
+//   });
+//   return data;
+// }
 
 // TODO: temporary getwords to test. Change this later
 export async function getWords(): Promise<Word[]> {
@@ -58,98 +58,98 @@ export async function getWordListWords(wordListID: string): Promise<Word[]> {
   return words;
 }
 
-export async function getUserQuizzes(userID: string) {
-  const data = await prisma.userQuizProgress.findMany({
-    where: { userId: userID },
-    select: { quizQuizId: true, learnCompleted: true },
-  });
-  return data;
-}
+// export async function getUserQuizzes(userID: string) {
+//   const data = await prisma.userQuizProgress.findMany({
+//     where: { userId: userID },
+//     select: { quizQuizId: true, learnCompleted: true },
+//   });
+//   return data;
+// }
 
-export async function getUserQuizProgress(
-  userID: string,
-  quizID: string,
-): Promise<UserQuizProgressWithQuiz | null> {
-  const data = await prisma.userQuizProgress.findFirst({
-    where: { userId: userID, quizQuizId: quizID },
-    include: {
-      quiz: {
-        include: {
-          questions: {
-            include: {
-              answers: true,
-              userQuestionProgress: {
-                where: {
-                  userId: userID,
-                },
-              },
-            },
-            orderBy: {
-              questionId: "asc",
-            },
-          },
-        },
-      },
-    },
-  });
-  return data;
-}
+// export async function getUserQuizProgress(
+//   userID: string,
+//   quizID: string,
+// ): Promise<UserQuizProgressWithQuiz | null> {
+//   const data = await prisma.userQuizProgress.findFirst({
+//     where: { userId: userID, quizQuizId: quizID },
+//     include: {
+//       quiz: {
+//         include: {
+//           questions: {
+//             include: {
+//               answers: true,
+//               userQuestionProgress: {
+//                 where: {
+//                   userId: userID,
+//                 },
+//               },
+//             },
+//             orderBy: {
+//               questionId: "asc",
+//             },
+//           },
+//         },
+//       },
+//     },
+//   });
+//   return data;
+// }
 
-export async function getQuiz(quizID: string) {
-  const quiz = await prisma.quiz.findFirst({
-    where: {
-      quizId: quizID,
-    },
-    include: {
-      questions: {
-        include: {
-          answers: true,
-        },
-        orderBy: {
-          questionId: "asc",
-        },
-      },
-    },
-  });
-  if (!quiz) {
-    throw Error("Quiz not found");
-  }
-  return quiz;
-}
+// export async function getQuiz(quizID: string) {
+//   const quiz = await prisma.quiz.findFirst({
+//     where: {
+//       quizId: quizID,
+//     },
+//     include: {
+//       questions: {
+//         include: {
+//           answers: true,
+//         },
+//         orderBy: {
+//           questionId: "asc",
+//         },
+//       },
+//     },
+//   });
+//   if (!quiz) {
+//     throw Error("Quiz not found");
+//   }
+//   return quiz;
+// }
 
-export async function getQuestions(quizID: string) {
-  const quiz = await prisma.quiz.findFirst({
-    where: {
-      quizId: quizID,
-    },
-    include: {
-      questions: {
-        include: {
-          answers: true,
-        },
-        orderBy: {
-          questionId: "asc",
-        },
-      },
-    },
-  });
-  if (!quiz) {
-    throw Error("Quiz not found");
-  }
-  return quiz.questions;
-}
+// export async function getQuestions(quizID: string) {
+//   const quiz = await prisma.quiz.findFirst({
+//     where: {
+//       quizId: quizID,
+//     },
+//     include: {
+//       questions: {
+//         include: {
+//           answers: true,
+//         },
+//         orderBy: {
+//           questionId: "asc",
+//         },
+//       },
+//     },
+//   });
+//   if (!quiz) {
+//     throw Error("Quiz not found");
+//   }
+//   return quiz.questions;
+// }
 
-export async function getQuizWords(quizID: string) {
-  const quiz = await prisma.quiz.findFirst({
-    where: {
-      quizId: quizID,
-    },
-    select: {
-      words: true,
-    },
-  });
-  if (!quiz) {
-    throw new Error("Quiz not found");
-  }
-  return quiz.words;
-}
+// export async function getQuizWords(quizID: string) {
+//   const quiz = await prisma.quiz.findFirst({
+//     where: {
+//       quizId: quizID,
+//     },
+//     select: {
+//       words: true,
+//     },
+//   });
+//   if (!quiz) {
+//     throw new Error("Quiz not found");
+//   }
+//   return quiz.words;
+// }
