f366adc06263d68f189b57e1b651d4eba176d9e9
Ian1528 <102779366+Ian1528@users.noreply.github.com>
2024-08-19 21:48:59 -0500

    fixed build

--- frontend/app/class/[classID]/page.tsx
+++ frontend/app/class/[classID]/page.tsx
@@ -1,6 +1,5 @@
 import ClassPage from "@/components/class/classPage";
 import StudentClassPage from "@/components/class/studentClassPage"
-import { CheckboxReactHookFormMultiple } from "@/components/wordList/formTest";
 
 export default async function Page({ params }: { params: { classID: string } }) {
   const classString = params.classID;
