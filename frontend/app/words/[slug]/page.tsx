0a1767d2c90c0c4b36e4544f1f87d1afe3da8e02
Ian1528 <102779366+Ian1528@users.noreply.github.com>
2024-09-10 22:41:34 -0400

    changed color of words page

--- frontend/app/words/[slug]/page.tsx
+++ frontend/app/words/[slug]/page.tsx
@@ -7,7 +7,7 @@ const words = await getWords(params.slug);
     <div className="flex items-center justify-center mt-4">
       <div className="mx-auto space-y-6 rounded-lg">
         <div className="space-y-2 text-center">
-          <h1 className="text-3xl font-bold text-[#00B894]">Words</h1>
+          <h1 className="text-3xl font-bold text-[#ff6b6b]">Words</h1>
           <p className="text-gray-600">
             Learn new words and their definitions
           </p>
@@ -25,7 +25,7 @@ const words = await getWords(params.slug);
 
 function Word({ word, definition }: { word: string, definition: string }) {
   return (
-    <div className="flex w-fullitems-center justify-between bg-white p-4 rounded-md dark:bg-[#00D1B2] border-gray-600 border-2">
+    <div className="flex w-fullitems-center justify-between bg-white p-4 rounded-md dark:bg-[#ff6b6b] border-gray-600 border-2">
       <div className="flex items-center gap-4">
         <div className="text-lg text-gray-600 font-semibold">{word}</div>
         <div className="text-sm text-gray-600">
